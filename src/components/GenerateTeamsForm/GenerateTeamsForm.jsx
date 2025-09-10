eba44ce2d6ce45231c3633cb18ca84879f860e0e
Logan Masniuk <logan.masniuk@gmail.com>
2025-09-10 16:18:14 -0700

    remove unused imports

--- src/components/GenerateTeamsForm/GenerateTeamsForm.jsx
+++ src/components/GenerateTeamsForm/GenerateTeamsForm.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import "./GenerateTeamsForm.scss";
 import convertCsvToJson from "../../utils/csvToJson";
--- src/components/TeamResults/TeamResults.jsx
+++ src/components/TeamResults/TeamResults.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./TeamResults.scss";
 
 export default function TeamResults({ teams }) {
